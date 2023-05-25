05aa17c379a789ce09d6a244a1397b36d3ed1cb3
bjorgeh <19abh92@gmail.com>
2023-05-25 11:49:42 +0200

    Lagt til kommentarer

--- load_from_file_dialog.cpp
+++ load_from_file_dialog.cpp
@@ -1,20 +1,23 @@
 #include "load_from_file_dialog.h"
 #include "ui_load_from_file_dialog.h"
-
+// k
 load_from_file_Dialog::load_from_file_Dialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::load_from_file_Dialog)
 {
+    //Setter opp vidnuet, endrer navn, disabler knapp..
     ui->setupUi(this);
     ui->pushButton->setDisabled(true);
     ui->lineEdit->setPlaceholderText(tr("Filename"));
+    this->setWindowTitle("Load from file");
 }
-
+// ~
 load_from_file_Dialog::~load_from_file_Dialog()
 {
     delete ui;
 }
 
+//Lukker vindu når knanpp trykkes
 void load_from_file_Dialog::on_pushButton_2_clicked()
 {
     this->close();
@@ -22,26 +25,33 @@ void load_from_file_Dialog::on_pushButton_2_clicked()
 
 void load_from_file_Dialog::on_pushButton_3_clicked()
 {
-    path = QFileDialog::getOpenFileName(this, tr("Open a file"));
+    //Åpner browse vindu for å velge fil
+    path = QFileDialog::getOpenFileName(this, tr("Open json file"));
+    //Lager QFile av filen med path
     QFile file(path);
+    //Lager fileinfo objekt for å få bare filnavnet
     QFileInfo *fileInfo = new QFileInfo(path);
     QString fileName = fileInfo->fileName();
 
+    //Hvis fil ikke kan leses viser error
     if(!file.open(QIODevice::ReadOnly))
     {
-        QMessageBox::critical(this, tr("Error"), file.errorString());
+        QMessageBox::critical(this, tr("File error"), file.errorString());
         return;
     }
+    //Setter text i line edit til bare filnavnet
     ui->lineEdit->setPlaceholderText(fileName);
 
+    //Sjekker om .json finnes i filnavnet
     if(fileName.contains(".json"))
         {
             qInfo() << tr("json file found");
+            //aktiverer add knapp
             ui->pushButton->setDisabled(false);
         }
 }
 
-
+//Sender path fil mainwindow og lukker vindu
 void load_from_file_Dialog::on_pushButton_clicked()
 {
     emit filePathSignal(path);
--- load_from_file_dialog.h
+++ load_from_file_dialog.h
@@ -11,28 +11,30 @@ namespace Ui {
 class load_from_file_Dialog;
 }
 
+//Lager klassen
 class load_from_file_Dialog : public QDialog
 {
     Q_OBJECT
 
 public:
+    //konstruktør & dekonstruktør
     explicit load_from_file_Dialog(QWidget *parent = nullptr);
     ~load_from_file_Dialog();
 
 private slots:
+    //deklarerer slot funksjoner
     void on_pushButton_2_clicked();
-
     void on_pushButton_3_clicked();
-
     void on_pushButton_clicked();
 
 private:
     Ui::load_from_file_Dialog *ui;
+    //string for path fil json fil
     QString path;
 
 signals:
+    //sender signal med path til mainwindow
     QString filePathSignal(QString path);
-
 };
 
 #endif // LOAD_FROM_FILE_DIALOG_H
